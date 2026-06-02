1a73a015ea44978fccce9d786333029f75f38776
Jonathan Schmittat <194770151+HQ2000-Rust@users.noreply.github.com>
2026-06-03 00:18:01 +0200

    add wip EntityMetadata (out)

---
+++ crates/tachyoo_protocol/src/out/types/entity_metadata.rs
@@ -0,0 +1,11 @@
+use crate::out::types::{UByte, var::int::VarInt};
+
+/*
+pub struct EntityMeta {
+    index: UByte,
+    type_: Option<VarInt>, //enum
+    value: Option<
+
+}
+*/
+// a big enum...or codegen
