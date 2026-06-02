54186b7b08a8db614d5556d66d9fd6014475b0c2
Jonathan Schmittat <194770151+HQ2000-Rust@users.noreply.github.com>
2026-06-02 03:00:39 +0200

    add String out impl

--- crates/tachyoo_protocol/src/out/types/string.rs
+++ crates/tachyoo_protocol/src/out/types/string.rs
@@ -1,3 +1,24 @@
-//TODO: length encoding
-//it's actually String(n), maybe partially encode that?
-pub struct String {}
+use crate::out::types::var::int::{VarInt, var_int};
+
+//better name!
+pub struct String_ {
+    //max: 32767
+    len: VarInt,
+    data: Box<str>,
+}
+
+impl String_ {
+    const MAX_LENGTH: usize = 32767;
+}
+
+//TODO: maybe impl Into<Box<str>>??
+pub fn string(string: String) -> Option<String_> {
+    if string.len() > String_::MAX_LENGTH {
+        None
+    } else {
+        Some(String_ {
+            len: var_int(string.len() as i32),
+            data: string.into_boxed_str(),
+        })
+    }
+}
