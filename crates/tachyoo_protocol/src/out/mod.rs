0d755d0032b919af1837ca13166f0268f52b3fa0
Jonathan Schmittat <194770151+HQ2000-Rust@users.noreply.github.com>
2026-06-02 02:31:10 +0200

    add WIP variadics impl, remove `Transfer for T where T: AsRef<[u8]>` imp

--- crates/tachyoo_protocol/src/out/mod.rs
+++ crates/tachyoo_protocol/src/out/mod.rs
@@ -1,7 +1,37 @@
+use tokio::io::{self, AsyncWriteExt};
+
 pub(super) mod raw_packet;
 pub mod types;
 
 //types that are reprs for transmission
-pub trait Transfer: AsRef<[u8]> {}
+#[async_trait::async_trait] //TODO: tmp
+pub trait Transfer: AsRef<[u8]> {
+    async fn write_to_tcp_stream(&self, mut stream: tokio::net::TcpStream) -> Result<(), io::Error> {
+        //for cancel safety (FIXME)
+        stream.write_all_buf(&mut self.as_ref()).await?;
+        Ok(())
+    }
+}
 
-impl<T> Transfer for T where T: AsRef<[u8]> {}
+
+/*
+macro_rules! impl_transfer {
+($N:expr, $($T:ident),*) => {
+        impl<$($T),*> Transfer for ($($T,)*)
+       where T: Transfer,
+        {
+           // type Tup = ($(Foo<$T>,)*);
+
+            fn write_to_tcp_stream(&self, mut stream: tokio::net::TcpStream) -> Result< {
+                
+                tokio::try_join!(
+                    stream.write_all_buf(&mut self.$N.as_ref()),
+            
+                )
+            }
+        }
+    };
+}
+
+variadics_please::all_tuples!(impl_transfer, 1, 15, T);
+*/
