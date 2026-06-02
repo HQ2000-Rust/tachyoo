0050c93187037b4c1e551e70b5382e307865cb3b
Jonathan Schmittat <194770151+HQ2000-Rust@users.noreply.github.com>
2026-06-03 00:14:26 +0200

    create Var types file (in)

---
+++ crates/tachyoo_protocol/src/in_/var.rs
@@ -0,0 +1,6 @@
+    #[derive(Debug, thiserror::Error)]
+    #[error("the VarInt wouldn't fit into 32 bits")]
+    pub struct VarIntError(());
+    #[derive(Debug, thiserror::Error)]
+    #[error("the VarLong wouldn't fit into 64 bits")]
+    pub struct VarLongError(());
