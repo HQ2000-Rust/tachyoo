bb84b2dcb7d5d9e53045bd61412a0bf59841c616
Jonathan Schmittat <194770151+HQ2000-Rust@users.noreply.github.com>
2026-06-04 00:05:49 +0200

    remove IntoTransferable (forgot to commit earlier)

--- crates/tachyoo_protocol/src/out/mod.rs
+++ crates/tachyoo_protocol/src/out/mod.rs
@@ -1,31 +1,34 @@
-use tokio::io::{self, AsyncWriteExt};
+use tokio::io::{self, AsyncWrite, AsyncWriteExt};
 
 pub(super) mod raw_packet;
 pub mod types;
 
 //maybe still just methods????
 //trait for types that implement (fallible) conversion into transferable types
-pub trait IntoTransferable {
+/*pub trait IntoTransferable {
     type Transferable: Transfer;
     type Error: std::error::Error;
     /* async */
     fn try_into_transferable(self) -> Result<Self::Transferable, Self::Error>;
-}
+}*/
 
 //TODO: define cancel safety requirements
-//types that are reprs for transmission
+//types that can be infallibly transmitted (written to a tcp stream)
+// maybe just something writable, so less mistakes could happen?
 #[async_trait::async_trait] //TODO: tmp
 pub trait Transfer {
-    async fn write_to_tcp_stream(&self, stream: tokio::net::TcpStream) -> Result<(), io::Error>;
+    //async fn write_to_tcp_stream(&self, stream: tokio::net::TcpStream) -> Result<(), io::Error>;
 
-    /*    async fn write_to_tcp_stream(
-        &self,
-        mut stream: tokio::net::TcpStream,
-    ) -> Result<(), io::Error> {
-        //for cancel safety (FIXME)
-        stream.write_all_buf(&mut self.as_bytes()).await?;
-        Ok(())
-    }*/
+    async fn write_data(&self, writeable: &mut Writable) -> io::Result<()>;
+}
+
+pub struct Writable(tokio::net::TcpStream);
+
+impl Writable {
+    //TODO: more efficient: not only slices
+    async fn write_all(&mut self, src: &[u8]) -> io::Result<()> {
+        self.0.write_all(src).await
+    }
 }
 
 /*
