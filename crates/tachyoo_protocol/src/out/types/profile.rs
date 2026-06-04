7447041f03d2a4404a284b6767607c981b883167
Jonathan Schmittat <194770151+HQ2000-Rust@users.noreply.github.com>
2026-06-04 17:49:04 +0200

    GameProfile/ResolvableProfile out impls

--- crates/tachyoo_protocol/src/out/types/profile.rs
+++ crates/tachyoo_protocol/src/out/types/profile.rs
@@ -1,20 +1,25 @@
 use tokio::io;
 
-use crate::out::{Transfer, Writable, types::{UUID, array::PrefixedArray, option::prefixed::PrefixedOptional, string::String_, var::int::VarInt}};
+use crate::out::{Transfer, Writable, types::{UUID, array::PrefixedArray, identifier::Identifier, option::{prefixed::PrefixedOptional, unprefixed::Optional}, string::String_, var::int::VarInt}};
 
 
 
 pub struct ResolvableProfile {
     profile_kind: VarInt,
     unpack: Unpack,
+    body: Optional<Identifier>,
+    cape: Optional<Identifier>,
+    elytra: Optional<Identifier>,
+    //wide=0, slim=1
+    model: Optional<VarInt>,
 }
 
-pub enum Unpack {
+enum Unpack {
     Partial {
         username: PrefixedOptional<String_<16>>,
         uuid: PrefixedOptional<UUID>,
-        //array: max 16??
-        properties: PrefixedArray<(String_<64>, String_<32767>, PrefixedOptional<String_<1024>>)>
+        //max 16 (TODO)
+        props: PrefixedArray<GameProfileProp>,
     },
     Complete(GameProfile)
 }
@@ -30,5 +35,60 @@ impl Transfer for Unpack {
                 properties.write_data(writeable).await?;
             }
         }
+    }
+}
+
+impl ResolvableProfile {
+    //TODO: as needed
+    //pub fn new
+}
+
+#[async_trait::async_trait]
+impl Transfer for ResolvableProfile {
+    async fn write_data(&self, writeable: &mut Writable) -> io::Result<()> {
+        self.profile_kind.write_data(writeable).await?;
+        self.unpack.write_data(writeable).await?;
+        self.body.write_data(writeable).await?;
+        self.cape.write_data(writeable).await?;
+        self.elytra.write_data(writeable).await?;
+        self.model.write_data(writeable).await?;
+
+        Ok(())
+    }
+}
+
+pub struct GameProfile {
+    uuid: UUID,
+    username: String_<16>,
+    //max 16 (TODO again)
+    props: PrefixedArray<GameProfileProp>,
+}
+
+
+struct GameProfileProp {
+    name:
+    String_<64>, val: String_<32767>, signature: PrefixedOptional<String_<1024>>
+    
+}
+
+#[async_trait::async_trait]
+impl Transfer for GameProfileProp {
+    async fn write_data(&self, writeable: &mut Writable) -> io::Result<()> {
+        self.name.write_data(writeable).await?;
+        self.val.write_data(writeable).await?;
+        self.signature.write_data(writeable).await?;
+
+        Ok(())
+    }
+}
+
+#[async_trait::async_trait]
+impl Transfer for GameProfile {
+    async fn write_data(&self, writeable: &mut Writable) -> io::Result<()> {
+        self.uuid.write_data(writeable).await?;
+        self.username.write_data(writeable).await?;
+        self.props.write_data(writeable).await?;
+
+        Ok(())
     }
 }
