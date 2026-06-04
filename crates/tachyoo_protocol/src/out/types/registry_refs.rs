f93e3afcdd77d24d477aae93da5a0eea172ef505
Jonathan Schmittat <194770151+HQ2000-Rust@users.noreply.github.com>
2026-06-04 14:48:27 +0200

    complete IdOrX / IdSet impl

--- crates/tachyoo_protocol/src/out/types/registry_refs.rs
+++ crates/tachyoo_protocol/src/out/types/registry_refs.rs
@@ -1,4 +1,14 @@
-use crate::out::{Transfer, types::{array::Array, identifier::{self, Identifier}, option::unprefixed::Optional, var::int::VarInt}};
+use tokio::io;
+
+use crate::out::{
+    Transfer, Writable,
+    types::{
+        array::Array,
+        identifier::Identifier,
+        option::unprefixed::Optional,
+        var::int::VarInt,
+    },
+};
 
 //TODO: would enums be better?
 
@@ -11,11 +21,30 @@ pub struct IdOrX<X> {
 impl<X> IdOrX<X> {
     //TODO: determine valid registry id range!!
     pub fn id(id: i32) -> IdOrX<X> {
-        IdOrX { id: VarInt::new(id +1), x: Optional::none() }
+        IdOrX {
+            id: VarInt::new(id + 1),
+            x: Optional::none(),
+        }
     }
 
     pub fn x(x: X) -> IdOrX<X> {
-        IdOrX { id: VarInt::new(0), x: Optional::some(x) }
+        IdOrX {
+            id: VarInt::new(0),
+            x: Optional::some(x),
+        }
+    }
+}
+
+#[async_trait::async_trait]
+impl<X> Transfer for IdOrX<X>
+where
+    X: Transfer + Send + Sync,
+{
+    async fn write_data(&self, writeable: &mut Writable) -> io::Result<()> {
+        self.id.write_data(writeable).await?;
+        self.x.write_data(writeable).await?;
+
+        Ok(())
     }
 }
 
@@ -26,13 +55,32 @@ pub struct IdSet {
 }
 
 impl IdSet {
-    pub fn named_id_set(tag_name: Identifier) -> IdSet  {
-        IdSet { ty: VarInt::new(0), tag_name: Optional::some(tag_name), ids: Optional::none() }
+    pub fn named_id_set(tag_name: Identifier) -> IdSet {
+        IdSet {
+            ty: VarInt::new(0),
+            tag_name: Optional::some(tag_name),
+            ids: Optional::none(),
+        }
     }
 
     pub fn inline_ids(ids: impl Iterator<Item = VarInt>) -> IdSet {
-        let array=Array::from_iter(ids);
+        let array = Array::from_iter(ids);
         //TODO: ensure no overflow occurs
-        IdSet { ty: VarInt::new(array.len() as i32 + 1), tag_name: Optional::none(), ids: Optional::some(array) }
-    } 
+        IdSet {
+            ty: VarInt::new(array.len() as i32 + 1),
+            tag_name: Optional::none(),
+            ids: Optional::some(array),
+        }
+    }
+}
+
+#[async_trait::async_trait]
+impl Transfer for IdSet {
+    async fn write_data(&self, writeable: &mut Writable) -> io::Result<()> {
+        self.ty.write_data(writeable).await?;
+        self.tag_name.write_data(writeable).await?;
+        self.ids.write_data(writeable).await?;
+
+        Ok(())
+    }
 }
