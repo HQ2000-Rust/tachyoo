62c61fa827edc356b87c6ead166bb6dc69efc728
Jonathan Schmittat <194770151+HQ2000-Rust@users.noreply.github.com>
2026-06-08 15:34:50 +0200

    make the Transfer trait sync

--- crates/tachyoo_protocol/src/out/mod.rs
+++ crates/tachyoo_protocol/src/out/mod.rs
@@ -1,24 +1,26 @@
-use tokio::io::{self, AsyncWriteExt};
 
-pub(super) mod raw_packet;
+pub(super) mod packet;
 pub mod types;
 
-//TODO: define cancel safety requirements
-//types that can be infallibly transmitted (written to a tcp stream)
-// maybe just something writable, so less mistakes could happen?
-#[async_trait::async_trait]
+//types that can be infallibly transmitted
 pub trait Transfer {
-    async fn write_data(&self, writeable: &mut Writable) -> io::Result<()>;
+    fn write_bytes(&self, buffer: &mut [u8]);
 }
 
-pub struct Writable(tokio::net::TcpStream);
 
-impl Writable {
-    //TODO: more efficient: not only slices
-    async fn write_all(&mut self, src: &[u8]) -> io::Result<()> {
-        self.0.write_all(src).await
-    }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /*
 macro_rules! impl_transfer {
