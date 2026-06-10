fc2cb38f06de25e50afd9c418800b6255d274dc1
Jonathan Schmittat <194770151+HQ2000-Rust@users.noreply.github.com>
2026-06-10 17:53:40 +0200

    add stub protocol parser

--- crates/tachyoo_protocol/src/in_/mod.rs
+++ crates/tachyoo_protocol/src/in_/mod.rs
@@ -1,1 +1,21 @@
+pub mod state;
+pub mod types;
+pub mod var;
 
+use bytes::{BufMut, BytesMut};
+use tokio::io::{self, AsyncReadExt};
+
+pub struct ProtocolParser {
+    buffer: BytesMut,
+}
+
+impl ProtocolParser {
+    //TODO: hand written future? (also, optimize in general!)
+    pub async fn read<R: AsyncReadExt + Unpin>(&mut self, w: &mut R) -> io::Result<()> {
+        let mut buf=Vec::new();
+        w.read_to_end(&mut buf).await?;
+        self.buffer.extend_from_slice(buf.as_slice());
+        Ok(())
+        
+    }
+}
