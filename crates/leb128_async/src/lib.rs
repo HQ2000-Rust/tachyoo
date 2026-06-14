c6d352c613ce4f73ca5f91d0fe99f1889e8a2052
Jonathan Schmittat <194770151+HQ2000-Rust@users.noreply.github.com>
2026-06-14 21:14:00 +0200

    add stub leb128_async crate

---
+++ crates/leb128_async/src/lib.rs
@@ -0,0 +1,32 @@
+pub mod read {
+    pub mod signed {
+        use std::io::{self, ErrorKind};
+
+use leb128::read::Error::{IoError, Overflow};
+use tokio::io::AsyncReadExt;
+
+        pub async fn read<R: AsyncReadExt + Unpin>(reader: &mut R) -> io::Result<i64> {
+            let buf=[0u8;10];
+
+            
+            
+            let mut size = 1;
+            let decoded = loop {
+                reader.read_exact(&mut buf[(size-1)..size]).await?;
+                
+                match leb128::read::signed(&mut &buf[..size]) {
+                    Err(IoError(e)) if e.kind()==ErrorKind::UnexpectedEof => size += 1,
+                    Err(IoError(e)) => return Err(e),
+                    Ok(decoded_) => break decoded_,
+                    Err(Overflow) => panic!(),
+                }
+            };
+
+            Ok(decoded)
+        }
+    }
+
+    pub mod unsigned {
+        
+    }
+}
---
+++ crates/tachyoo_protocol/src/in_/types/string.rs
@@ -0,0 +1,65 @@
+fn validate(data: &u32) -> Result<(), McStringError> {
+    
+}
+
+
+pub struct McString<const MAX_LEN: u16> {
+    data: Box<str>,
+}
+
+//TODO: move inside the module again?
+pub const ABSOLUTE_MAX_LENGTH: u16 = 32767;
+
+impl<const MAX_LENGTH: u16> McString<MAX_LENGTH> {
+    const __ASSERTION: () = assert!(MAX_LENGTH <= ABSOLUTE_MAX_LENGTH);
+
+    fn new(str: impl AsRef<str>) -> Result<u16, McStringError> {
+
+    }
+
+    pub fn len(&self) -> u16 {
+        //invariant!!
+        self.data.len() as u16
+    }
+}
+
+impl<const MAX_LEN: u16> AsRef<str> for McString<MAX_LEN> {
+    fn as_ref(&self) -> &str {
+        &self.data
+    }
+}
+
+//TODO: collapse both impl into one function (mostly)
+impl<const MAX_LENGTH: u16> TryFrom<String> for McString<MAX_LENGTH> {
+    type Error = McStringError;
+
+    fn try_from(string: String) -> Result<Self, Self::Error> {
+        let len = McString::<MAX_LENGTH>::is_valid_and_len(string.as_str())?;
+
+        let len = VarInt::new(len.into());
+
+        Ok(McString {
+            len,
+            data: string.into_boxed_str(),
+        })
+    }
+}
+
+impl<const MAX_LENGTH: u16> TryFrom<Box<str>> for McString<MAX_LENGTH> {
+    type Error = McStringError;
+
+    fn try_from(string: Box<str>) -> Result<Self, Self::Error> {
+        let len = McString::<MAX_LENGTH>::is_valid_and_len(&*string)?;
+
+        Ok(McString { data: string })
+    }
+}
+
+#[derive(Debug, Clone, thiserror::Error)]
+#[error("Not a valid McString")]
+pub struct McStringError(());
+
+pub struct McStr<'a> {
+    data: &'a str,
+}
+
