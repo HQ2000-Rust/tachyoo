69c86c358f2177ed18d338c92021d2f6273e12c4
Jonathan Schmittat <194770151+HQ2000-Rust@users.noreply.github.com>
2026-06-15 16:45:33 +0200

    improve mcstr

--- crates/tachyoo_protocol/src/in_/types/string/str.rs
+++ crates/tachyoo_protocol/src/in_/types/string/str.rs
@@ -1,22 +1,37 @@
-use crate::in_::types::string::McStringError;
+use crate::{in_::types::string::McStringError, util::string::ABSOLUTE_MAX_LEN};
 
+#[derive(Clone, Debug, Copy, PartialEq, Eq)]
 pub struct McStr<'a, const MAX_LENGTH: u16> {
     data: &'a str,
 }
 
 impl<'a, const MAX_LEN: u16> McStr<'a, MAX_LEN> {
     //TODO: fix
-    pub const ABSOLUTE_MAX_LEN: u16 = 200000;
+    const __ASSERTION: () = assert!(MAX_LEN <= ABSOLUTE_MAX_LEN);
 
     fn new(bytes: &[u8]) -> Result<McStr<'_, MAX_LEN>, McStringError> {
         let str = str::from_utf8(bytes).map_err(McStringError::InvalidUtf8)?;
 
-        let (valid, len) = crate::util::string::is_valid_and_len::<MAX_LEN, MAX_LEN>(str);
+        let (valid, len) = crate::util::string::is_valid_and_len::<MAX_LEN>(str);
 
         if valid {
             Ok(McStr { data: str })
         } else {
             Err(McStringError::TooLong { len })
         }
+    }
+
+    pub fn as_str(&self) -> &str {
+        self.data
+    }
+
+    pub fn len(&self) -> u16 {
+        self.data.len() as u16
+    }
+}
+
+impl<const MAX_LEN: u16> AsRef<str> for McStr<'_, MAX_LEN> {
+    fn as_ref(&self) -> &str {
+        self.data
     }
 }
